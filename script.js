d431afc0a36a7f9ed476c8bb3d8f4349bbd234cf
Eliyahalili123 <elizahalili63@gmail.com>
2025-04-11 23:50:03 +0200

    Ngarkimi i parë i projektit tim

---
+++ script.js
@@ -0,0 +1,34 @@
+          <div class="testimonial-card">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i> <!-- 4 stars -->
+            </div>
+            <p>"Përkushtimi dhe preciziteti janë sinonim i punës, te lumtë Sinan"</p>
+            <h4>- Bekim H.</h4>
+          </div>
+          <div class="testimonial-card">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>"Puna flet vet! Suksese Dekor Plast"</p>
+            <h4>- Vedat A.</h4>
+          </div>
+           <div class="testimonial-card">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>"Të lumshin duart Sinan! Që 6 vite mi ke montuar në shtëpinë time, as edhe më të voglin defekt se kom pas!"</p>
+            <h4>- Fatmir I.</h4>
+          </div>
+         
